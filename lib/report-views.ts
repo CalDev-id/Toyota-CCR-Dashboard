3e4bc700e65d4bbd823e1ebe440a002f36df9a09
Heical Chandra <haical31@gmail.com>
2026-07-17 15:14:04 +0700

    Update report-views.ts

--- lib/report-views.ts
+++ lib/report-views.ts
@@ -1,3 +1,7 @@
 export function summaryViewName(baseName: string) {
+  if (baseName === "v_assy_summary") {
+    return baseName;
+  }
+
   return `${baseName}${process.env.REPORT_SUMMARY_VIEW_SUFFIX ?? ""}`;
 }
